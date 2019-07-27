62d6618130402532f8cb66da6c32eb34a07d5cf1
Tianyang Ding <tianyang.ding@gmail.com>
2019-07-27 15:49:37 +0800

    Finish Proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -18,12 +18,15 @@ public class Planet {
 	}
 
 	public Planet(Planet p) {
+		/*
 		xxPos = p.xxPos;
 		yyPos = p.yyPos;
 		xxVel = p.xxVel;
 		yyVel = p.yyVel;
 		mass = p.mass;
 		imgFileName = p.imgFileName;
+		*/
+		this(p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
 	}
 
 	public double calcDistance(Planet p) {
