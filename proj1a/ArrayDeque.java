3b84cd171621b5b79343d6f8d2c5baa393ceb657
Tianyang Ding <tianyang.ding@gmail.com>
2020-02-08 01:05:13 -0600

    version 2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -14,21 +14,26 @@ public class ArrayDeque<T> {
 
     private void resize(int factor, String method) {
         if (method.equals("Up")) {
-            T[] tmp = (T[]) new Object[size * factor];
-            /** If and else can merge after observation.
-             if (nextFirst == items.length - 1) {
-             System.arraycopy(items, 0, tmp, 0, items.length);
-             } else {
-             System.arraycopy(items, nextFirst + 1, tmp, 0, items.length - 1 - nextFirst);
-             System.arraycopy(items, 0, tmp, items.length - 1 - nextFirst, nextFirst + 1);
-             }
-             */
-            System.arraycopy(items, nextFirst + 1, tmp, 0, items.length - 1 - nextFirst);
-            System.arraycopy(items, 0, tmp, items.length - 1 - nextFirst, nextFirst + 1);
+            T[] tmp = (T[]) new Object[items.length * factor];
+//            if (nextFirst == items.length - 1) {
+//                System.arraycopy(items, 0, tmp, 0, size);
+//            } else {
+//                System.arraycopy(items, nextFirst + 1, tmp, 0, items.length - 1 - nextFirst);
+//                System.arraycopy(items, 0, tmp, items.length - 1 - nextFirst, nextLast);
+//            }
+            for (int i = 0; i < size; i++) {
+                tmp[i] = get(i);
+            }
             items = tmp;
         }
         if (method.equals("Down")) {
-            T[] tmp = (T[]) new Object[size / factor];
+            T[] tmp = (T[]) new Object[items.length / factor];
+//            if (nextFirst < nextLast) {
+//                System.arraycopy(items, nextFirst + 1, tmp, 0, size);
+//            } else {
+//                System.arraycopy(items, nextFirst + 1, tmp, 0, items.length - 1 - nextFirst);
+//                System.arraycopy(items, 0, tmp, items.length - 1 - nextFirst, nextLast);
+//            }
             for (int i = 0; i < size; i++) {
                 tmp[i] = get(i);
             }
@@ -129,5 +134,37 @@ public class ArrayDeque<T> {
             return items[nextFirst + 1 + index];
         }
         return items[nextFirst + 1 + index - items.length];
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> A = new ArrayDeque<>();
+        A.addLast(0);
+        A.addLast(1);
+
+        A.addLast(2);
+        A.addLast(3);
+        A.addFirst(4);
+        A.addLast(5);
+        A.addFirst(6);
+        A.addLast(7);
+        A.addLast(8);
+        A.addFirst(9);
+        A.addFirst(10);
+
+        A.removeLast();
+        A.removeLast();
+        A.removeFirst();
+        A.removeFirst();
+        A.removeFirst();
+        A.removeFirst();
+        A.removeFirst();
+        A.removeFirst();
+
+        System.out.println(A.size());
+        System.out.println(A.get(0));
+        System.out.println();
+        A.printDeque();
+        System.out.println(A.isEmpty());
+        System.out.println("");
     }
 }
